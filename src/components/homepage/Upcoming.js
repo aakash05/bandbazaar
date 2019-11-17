6b5dfdc36c19405f92948ed158c190d82e6a3f14
aakash05 <aakashcool.rai@gmail.com>
2019-11-17 05:35:27 +0530

    Updated

--- src/components/homepage/Upcoming.js
+++ src/components/homepage/Upcoming.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './wave.css';
+import './Home.css';
+
 
 const rect = {
     height: '90%',
@@ -44,91 +46,132 @@ function Upcoming() {
                     <h2 style={{zIndex: '2',paddingTop: '7%',
                     fontSize: '50px'}}>Upcoming Gigs</h2>
                 </div>
+                
             </section>
-            <div className="row" style={{marginTop: '5%'}}>
-                <div className="col-md-2">
-                    <div style={rect}>
-                        <p>28-31 Aug 2019</p>
-                    </div>
+            <div class="bg">
+            <div class="container">
+               
+            
+                <div class="d-flex flex-column">
+                    <div class="p-2">
+                            <div className="row" style={{marginTop: '5%'}}>
+                                <div className="col-md-2">
+                                   <div style={rect} class="bg-warning">
+                                      <p>28-31 Aug 2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
+                                    <h4>NEW DELHI, IN</h4>
+                                    <h4>Tech Fest, Delhi University</h4>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={registerButton}>
+                                        <i className="fa fa-edit"></i>  Register
+                                    </button>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={ticketButton}>
+                                        <i className="fa fa-ticket"></i>  Ticket
+                                    </button>
+                                </div>
+
+                            </div>
+                
+                    </div>  
+
                 </div>
-                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
-                    <h4>NEW DELHI, IN</h4>
-                    <h4>Tech Fest, Delhi University</h4>
+                <div class="d-flex flex-column">
+                    <div class="p-2">
+                            <div className="row" style={{marginTop: '5%'}}>
+                                <div className="col-md-2">
+                                   <div style={rect} class="bg-warning">
+                                      <p>28-31 Aug 2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
+                                    <h4>NEW DELHI, IN</h4>
+                                    <h4>Tech Fest, Delhi University</h4>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={registerButton}>
+                                        <i className="fa fa-edit"></i>  Register
+                                    </button>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={ticketButton}>
+                                        <i className="fa fa-ticket"></i>  Ticket
+                                    </button>
+                                </div>
+
+                            </div>
+                
+                    </div>  
+
                 </div>
-                <div className="col-md-3">
-                    <button style={registerButton}>
-                        <i className="fa fa-edit"></i>  Register
-                    </button>
+
+
+            <div class="d-flex flex-column">
+                    <div class="p-2">
+                            <div className="row" style={{marginTop: '5%'}}>
+                                <div className="col-md-2">
+                                   <div style={rect} class="bg-warning">
+                                      <p>28-31 Aug 2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
+                                    <h4>NEW DELHI, IN</h4>
+                                    <h4>Tech Fest, Delhi University</h4>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={registerButton}>
+                                        <i className="fa fa-edit"></i>  Register
+                                    </button>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={ticketButton}>
+                                        <i className="fa fa-ticket"></i>  Ticket
+                                    </button>
+                                </div>
+
+                            </div>
+                
+                    </div>  
+
                 </div>
-                <div className="col-md-3">
-                    <button style={ticketButton}>
-                        <i className="fa fa-ticket"></i>  Ticket
-                    </button>
-                </div>
+
+                  <div class="d-flex flex-column rounded-sm">
+                    <div class="p-2">
+                            <div className="row" style={{marginTop: '5%'}}>
+                                <div className="col-md-2">
+                                   <div style={rect} class="bg-warning">
+                                      <p>28-31 Aug 2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
+                                    <h4>NEW DELHI, IN</h4>
+                                    <h4>Tech Fest, Delhi University</h4>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={registerButton}>
+                                        <i className="fa fa-edit"></i>  Register
+                                    </button>
+                                </div>
+                                <div className="col-md-3">
+                                    <button style={ticketButton}>
+                                        <i className="fa fa-ticket"></i>  Ticket
+                                    </button>
+                                </div>
+
+                            </div>
+                
+                    </div>  
+
+            
+                </div>         
             </div>
-            <div className="row" style={{marginTop: '2%'}}>
-                <div className="col-md-2">
-                    <div style={rect}>
-                        <p>28-31 Aug 2019</p>
-                    </div>
-                </div>
-                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
-                    <h4>NEW DELHI, IN</h4>
-                    <h4>Tech Fest, Delhi University</h4>
-                </div>
-                <div className="col-md-3">
-                    <button style={registerButton}>
-                        <i className="fa fa-edit"></i>  Register
-                    </button>
-                </div>
-                <div className="col-md-3">
-                    <button style={ticketButton}>
-                        <i className="fa fa-ticket"></i>  Ticket
-                    </button>
-                </div>
             </div>
-            <div className="row" style={{marginTop: '2%'}}>
-                <div className="col-md-2">
-                    <div style={rect}>
-                        <p>28-31 Aug 2019</p>
-                    </div>
-                </div>
-                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
-                    <h4>NEW DELHI, IN</h4>
-                    <h4>Tech Fest, Delhi University</h4>
-                </div>
-                <div className="col-md-3">
-                    <button style={registerButton}>
-                        <i className="fa fa-edit"></i>  Register
-                    </button>
-                </div>
-                <div className="col-md-3">
-                    <button style={ticketButton}>
-                        <i className="fa fa-ticket"></i>  Ticket
-                    </button>
-                </div>
-            </div>
-            <div className="row" style={{marginTop: '2%'}}>
-                <div className="col-md-2">
-                    <div style={rect}>
-                        <p>28-31 Aug 2019</p>
-                    </div>
-                </div>
-                <div className="col-md-4" style={{textAlign: 'left',color: '#474746'}}>
-                    <h4>NEW DELHI, IN</h4>
-                    <h4>Tech Fest, Delhi University</h4>
-                </div>
-                <div className="col-md-3">
-                    <button style={registerButton}>
-                        <i className="fa fa-edit"></i>  Register
-                    </button>
-                </div>
-                <div className="col-md-3">
-                    <button style={ticketButton}>
-                        <i className="fa fa-ticket"></i>  Ticket
-                    </button>
-                </div>
-            </div>
+            
+           
             <h5 style={headStyle}><b>Show All</b> <img src={require('../../assets/more.png')}
             alt="" style={{width: '16px', height: '16px'}} /></h5> 
             <hr style={{marginTop: '5%'}}/>
